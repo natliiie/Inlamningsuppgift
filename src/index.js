e626f64ece5857fc79a946020a53c864f36b8d76
Natalie Nilsson <natalie.nilsson2@learnet.se>
2022-09-08 14:24:49 +0200

    Routing added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Education from "./Routes/Education";
+import Family from "./Routes/Family";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="family" element={<Family />} />
+      <Route path="education" element={<Education />} />
+    </Routes>
+  </BrowserRouter>
 );
