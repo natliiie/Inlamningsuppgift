627d2682ab83027a8e17cd252151cc2dff0bae72
Natalie Nilsson <natalie.nilsson2@learnet.se>
2022-09-12 12:35:53 +0200

    Form with message presented on page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 import { Navbar, Form } from "./Components";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  const handleMessages = (message) => {
+    setMessages([...messages, message]);
+    console.log(messages);
+  };
+
   return (
     <div>
       <header>My header</header>
@@ -9,8 +17,15 @@ function App() {
       <div>This is me</div>
       {/* här vill jag ha info */}
       {/* här vill jag ha min form */}
-      <Form />
+      <Form handleMessages={handleMessages} />
       {/* här vill jag ha en div med de olika posterna */}
+      <div>
+        <ul>
+          {messages.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,39 +1,53 @@
 import React from 'react';
 import { useState } from 'react';
 
-const Form = () => {
+const Form = ({handleMessages}) => {
 
-const [comment, setComment] = useState('')
-const [comments, addComment] = useState([])
+const [mailMessage, setMailMessage] = useState({
+    fromMail: '',
+    fromMessage:''
+})
+// const [fromMessage, setMailMessage] = useState('')
 
 const handleSubmit = e => {
     //prevent the page from load onClick
     e.preventDefault();
-    addComment([...comments, comment])
-    setComment('')
+
+    //skicka mailet
+    handleMessages(mailMessage.fromMessage)
+
+}
+
+const handleChange = e => {
+    setMailMessage({...mailMessage, [e.target.name]: e.target.value})
 }
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <p>Post a comment on the page?</p>
+            <p>Send me a mail?</p>
+            
+            <form onSubmit={handleSubmit}
+            
+        >
+                <label>Email</label>
                 <input
                 type="text" 
+                name='fromMail'
+                placeholder="name@email.com"
+                value={mailMessage.fromMail}
+                onChange={handleChange}
+                />
+                <label>Message</label>
+                <input
+                type="text" 
+                name='fromMessage'
                 placeholder="Say Hi"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                ></input>
-                <button type='submit'>skicka</button>
+                value={mailMessage.fromMessage}
+                onChange={handleChange}
+                />
+                <button type='submit'>Send</button>
             </form>
-
-            {/* Flytta till annat ställe ?      */}
-            {[...comments].reverse().map((element, index) => {
-                return (
-                    <div key={index}>
-                        <h2>{element}</h2>
-                    </div>
-                )
-            })}
         </div>  
     );
-}
+    }
+
 export default Form;
