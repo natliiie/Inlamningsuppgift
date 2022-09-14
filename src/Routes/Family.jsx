8c238d47a300041bd099e2836fafcb378517d7f0
Natalie Nilsson <natalie.nilsson2@learnet.se>
2022-09-14 13:34:06 +0200

    Family-page updated

--- src/Routes/Family.jsx
+++ src/Routes/Family.jsx
@@ -3,11 +3,14 @@ import { Navbar } from '../Components';
 
 const Family = () => {
     return (
-        <div>
-        <header>Header</header>
+        <div className='body'>
         <Navbar />
-        <div>
-            This is my family
+        <div className="navStyle">
+            <img src="./images/kids.jpg" alt="" className='familyImg'/>
+            <p className="textStyle">
+                This could be my kids, its not. 
+                But i have as the picture two little ones, 
+                a little boy and a two year older girl.</p>
         </div>
       </div>
     );
